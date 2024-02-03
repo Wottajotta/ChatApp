754201b12945bf1dcb759a556d118c2a2c69fc9d
Vladislav <vlad.kharchenko.96@list.ru>
2024-02-03 16:57:41 +0500

    New functions

--- app/src/main/java/com/example/myapplication/LoginOtpActivity.java
+++ app/src/main/java/com/example/myapplication/LoginOtpActivity.java
@@ -1,14 +1,101 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.myapplication.utils.AndroidUtil;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class LoginOtpActivity extends AppCompatActivity {
+
+    String phoneNumber;
+    Long timeoutSeconds = 60L;
+    String verificationCode;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+
+    EditText otpInput;
+    ProgressBar progressBar;
+    Button nextBtn;
+    TextView resendOtpTextView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
+
+        otpInput = findViewById(R.id.login_otp);
+        nextBtn = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        resendOtpTextView = findViewById(R.id.resend_otp_textview);
+
+
+        // Получаем номер телефона из прошлой активности
+        phoneNumber = Objects.requireNonNull(getIntent().getExtras()).getString("phone");
+
+        sendOtp(phoneNumber);
+
+    }
+
+    // Проверка и отправка одноразового кода на телефон
+    void sendOtp(String phoneNumber) {
+        setInProgress(true);
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)
+                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signIn(phoneAuthCredential);
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                AndroidUtil.showToast(getApplicationContext(), "Не удалось отправить код!");
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                verificationCode = s;
+                                resendingToken = forceResendingToken;
+                                AndroidUtil.showToast(getApplicationContext(), "Код отправлен!");
+                                setInProgress(false);
+                            }
+                        });
+        PhoneAuthProvider.verifyPhoneNumber(builder.build());
+    }
+
+    void setInProgress(boolean inProgress) {
+        if(inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            nextBtn.setVisibility(View.GONE);
+        }
+        else{
+            progressBar.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    void signIn(PhoneAuthCredential phoneAuthCredential) {
+        // Авторизация и вход в следующую активность
     }
 }
--- app/src/main/java/com/example/myapplication/LoginPhoneNumberActivity.java
+++ app/src/main/java/com/example/myapplication/LoginPhoneNumberActivity.java
@@ -2,13 +2,47 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
 
 public class LoginPhoneNumberActivity extends AppCompatActivity {
+
+    CountryCodePicker countryCodePicker;
+    EditText phoneInput;
+    Button sendOtpBtn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_phone_number);
+
+        // Определяем переменные
+        countryCodePicker = findViewById(R.id.login_country_code);
+        phoneInput = findViewById(R.id.login_phone_number);
+        sendOtpBtn = findViewById(R.id.send_otp_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+        progressBar.setVisibility(View.GONE);
+
+        // Проверяем корректность ведённого номера телефона
+        countryCodePicker.registerCarrierNumberEditText(phoneInput);
+        sendOtpBtn.setOnClickListener(view -> {
+           if(!countryCodePicker.isValidFullNumber()) {
+               phoneInput.setError("Номер телефона недействителен!");
+               return;
+           }
+           // Записываем номер телефона под ключом phone и переходим к следующей активности
+           Intent intent = new Intent(LoginPhoneNumberActivity.this, LoginOtpActivity.class);
+           intent.putExtra("phone", countryCodePicker.getFullNumberWithPlus());
+           startActivity(intent);
+        });
+
     }
 }
