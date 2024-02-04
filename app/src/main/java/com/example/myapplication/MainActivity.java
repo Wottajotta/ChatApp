578d32d008909c000af40af9261b10aaa7b7ab26
Vladislav <vlad.kharchenko.96@list.ru>
2024-02-04 17:38:43 +0500

    New functions

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ImageButton searchButton;
+    ChatFragment chatFragment;
+    ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        searchButton = findViewById(R.id.main_search_btn);
+
+        // Метод для перехода в поиск пользователей (по нажатию кнопки)
+        searchButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SearchUserActivity.class)));
+
+        // Переход на выбранные пункты меню (чат/профиль)
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId()==R.id.menu_chat){
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, chatFragment).commit();
+            }
+            if(item.getItemId()==R.id.menu_profile){
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, profileFragment).commit();
+            }
+            return true;
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
     }
 }
